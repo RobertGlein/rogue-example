7c636b31d012f7b2e24df4e53eedf50784beaafd
Robért Glein <rglein@beck.hep.wisc.edu>
2019-06-07 15:59:41 -0500

    send two test objects to the same packatizer app

--- cpp_stream_receiver/src/rssi_client_datatx.cpp
+++ cpp_stream_receiver/src/rssi_client_datatx.cpp
@@ -10,6 +10,7 @@
 #include <rogue/interfaces/stream/FrameIterator.h>
 #include <rogue/interfaces/stream/Buffer.h>
 #include <rogue/Helpers.h>
+#include <rogue/ApiWrapper.h>
 
 //! Receive slave data, count frames and total bytes for example purposes.
 class TestSend : public rogue::interfaces::stream::Master {
@@ -53,7 +54,8 @@ class TestSend : public rogue::interfaces::stream::Master {
 
 
 int main (int argc, char **argv) {
-   uint8_t data[100];
+   uint32_t ksize = 4000000;//8000000;
+   uint8_t data[ksize];
    struct timeval last;
    struct timeval curr;
    struct timeval diff;
@@ -62,8 +64,24 @@ int main (int argc, char **argv) {
    uint64_t diffBytes;
    double bw;
 
+   uint8_t data1[ksize];
+   struct timeval last1;
+   struct timeval curr1;
+   struct timeval diff1;
+   double   timeDiff1;
+   uint64_t lastBytes1;
+   uint64_t diffBytes1;
+   double bw1;
+
+   //Create data
+   for (int i = 0; i <= ksize; i++)
+    {
+        data[i] = i;
+        data1[i] = ksize-i;
+    }
+
    // Create the UDP client, jumbo = true
-   rogue::protocols::udp::ClientPtr udp  = rogue::protocols::udp::Client::create("192.168.2.187",8194,true);
+   rogue::protocols::udp::ClientPtr udp  = rogue::protocols::udp::Client::create("172.30.0.128",8192,true);
    udp->setRxBufferCount(64); // Make enough room for 64 outstanding buffers
 
    // RSSI
@@ -80,7 +98,10 @@ int main (int argc, char **argv) {
 
    // Create a test source and connect to channel 1 of the packetizer
    boost::shared_ptr<TestSend> send = boost::make_shared<TestSend>();
-   streamConnect(send,pack->application(1))
+   streamConnect(send,pack->application(0));
+   // Create a test source and connect to channel 1 of the packetizer
+   boost::shared_ptr<TestSend> send1 = boost::make_shared<TestSend>();
+   streamConnect(send1,pack->application(0));
 
    // Start the rssi link
    rssi->start();
@@ -91,21 +112,25 @@ int main (int argc, char **argv) {
 
    while(1) {
 
-      send->myGenFrame(data,100);
-      sleep(1);
+      send->myGenFrame(data,ksize);
+      //sleep(1);
       gettimeofday(&curr,NULL);
-
       timersub(&curr,&last,&diff);
-
       diffBytes = send->txBytes - lastBytes;
       lastBytes = send->txBytes;
-
       timeDiff = (double)diff.tv_sec + ((double)diff.tv_usec / 1e6);
       bw = (((float)diffBytes * 8.0) / timeDiff) / 1e9;
-
       gettimeofday(&last,NULL);
-
       printf("RSSI = %i. TxLast=%i, TxCount=%i, TxTotal=%li, Bw=%f, DropRssi=%i, DropPack=%i\n",rssi->getOpen(),send->txLast,send->txCount,send->txBytes,bw,rssi->getDropCount(),pack->getDropCount());
+
+      send1->myGenFrame(data1,ksize);
+      gettimeofday(&curr1,NULL);
+      timersub(&curr1,&last1,&diff1);
+      diffBytes1 = send1->txBytes - lastBytes1;
+      lastBytes1 = send1->txBytes;
+      timeDiff1 = (double)diff1.tv_sec + ((double)diff1.tv_usec / 1e6);
+      bw1 = (((float)diffBytes1 * 8.0) / timeDiff1) / 1e9;
+      gettimeofday(&last1,NULL);
+      printf("RSSI = %i. TxLast=%i, TxCount=%i, TxTotal=%li, Bw=%f, DropRssi=%i, DropPack=%i\n\n",rssi->getOpen(),send1->txLast,send1->txCount,send1->txBytes,bw1,rssi->getDropCount(),pack->getDropCount());
    }
 }
-
