05b457acfd97e599d4a15fb6fe2456b98f112ce3
Robért Glein <rglein@beck.hep.wisc.edu>
2019-06-07 19:00:37 -0500

    add register access to trigger TX in PL

--- cpp_stream_receiver/src/rssi_client_datarx.cpp
+++ cpp_stream_receiver/src/rssi_client_datarx.cpp
@@ -10,6 +10,9 @@
 #include <rogue/interfaces/stream/FrameIterator.h>
 #include <rogue/interfaces/stream/Buffer.h>
 #include <rogue/Helpers.h>
+#include <rogue/protocols/srp/SrpV3.h>
+#include <rogue/interfaces/memory/Master.h>
+#include <rogue/interfaces/memory/Constants.h>
 
 //! Receive slave data, count frames and total bytes for example purposes.
 class TestSink : public rogue::interfaces::stream::Slave {
@@ -51,6 +54,7 @@ class TestSink : public rogue::interfaces::stream::Slave {
 
             // Copy some data
             memcpy(dst, src, size);
+            //printf("dst = %i\n",&dst);
 
             // Update destination pointer and source iterator
             dst  += size;
@@ -68,55 +72,54 @@ int main (int argc, char **argv) {
    uint64_t lastBytes;
    uint64_t diffBytes;
    double bw;
-   // rglein
-   // struct timeval last1;
-   // struct timeval curr1;
-   // struct timeval diff1;
-   // double   timeDiff1;
-   // uint64_t lastBytes1;
-   // uint64_t diffBytes1;
-   // double bw1;
 
    //rogue::Logging::setLevel(rogue::Logging::Debug);
 
    // Create the UDP client, jumbo = true
    rogue::protocols::udp::ClientPtr udp  = rogue::protocols::udp::Client::create("172.30.0.128",8192,true);
    udp->setRxBufferCount(64); // Make enough room for 64 outstanding buffers
-   // rglein: Create the UDP client, jumbo = true
-   // rogue::protocols::udp::ClientPtr udp1  = rogue::protocols::udp::Client::create("172.30.0.128",8192,true);
-   // udp1->setRxBufferCount(64); // Make enough room for 64 outstanding buffers
 
    // RSSI
    rogue::protocols::rssi::ClientPtr rssi = rogue::protocols::rssi::Client::create(udp->maxPayload());
-   // rglein: RSSI
-   // rogue::protocols::rssi::ClientPtr rssi1 = rogue::protocols::rssi::Client::create(udp1->maxPayload());
 
    // Packetizer, ibCrc = false, obCrc = true
    rogue::protocols::packetizer::CoreV2Ptr pack = rogue::protocols::packetizer::CoreV2::create(false,true,true);
-   // rglein: Packetizer, ibCrc = false, obCrc = true
-   // rogue::protocols::packetizer::CoreV2Ptr pack1 = rogue::protocols::packetizer::CoreV2::create(false,true,true);
 
    // Connect the RSSI engine to the UDP client
    streamConnectBiDir(udp,rssi->transport());
-   // rglein: Connect the RSSI engine to the UDP client
-   // streamConnectBiDir(udp1,rssi1->transport());
 
    // Connect the RSSI engine to the packetizer
    streamConnectBiDir(rssi->application(), pack->transport());
-   // rglein: Connect the RSSI engine to the packetizer
-   // streamConnectBiDir(rssi1->application(), pack1->transport());
 
-   // Create a test sink and connect to channel 1 of the packetizer
+   // Create a test sink and connect to channel 0 of the packetizer
    boost::shared_ptr<TestSink> sink = boost::make_shared<TestSink>();
    streamConnect(pack->application(1),sink);
-   // rglein: Create a test sink and connect to channel 1 of the packetizer
-   // boost::shared_ptr<TestSink> sink1 = boost::make_shared<TestSink>();
-   // streamConnect(pack1->application(1),sink1);
+
+   // Create an SRP master and connect it to the packetizer
+   rogue::protocols::srp::SrpV3Ptr srp = rogue::protocols::srp::SrpV3::create();
+   streamConnect(srp,pack->application(0));
+
+   // Create a memory master and connect it to the srp
+   rogue::interfaces::memory::MasterPtr mast = rogue::interfaces::memory::Master::create();
+   mast->setSlave(srp);
 
    // Start the rssi link
    rssi->start();
-   // Start the rssi link
-   // rssi1->start();
+
+   // SsiPrbsTx
+   printf("\n -- SsiPrbsTx wr/rd to trigger the VHDL module -------------------------- \n");
+   uint32_t tmp = 3;
+   while ( ! rssi->getOpen() ) {
+      sleep(1);
+      printf("Establishing link ...\n");
+   }
+   mast->setError(0);
+   mast->reqTransaction(0x00040000,4,&tmp,rogue::interfaces::memory::Write);
+   mast->reqTransaction(0x00040000,4,&tmp,rogue::interfaces::memory::Read);
+   printf("Reg 0x00040000: %d \n", tmp);
+   mast->waitTransaction(0);
+   printf("Register done. Error=0x%x\n",mast->getError());
+   printf("\n -- Start client data rx loop -------------------------- \n");
 
    // Loop forever showing counts
    lastBytes = 0;
@@ -137,17 +140,5 @@ int main (int argc, char **argv) {
       gettimeofday(&last,NULL);
 
       printf("RSSI = %i. RxLast=%i, RxCount=%i, RxTotal=%li, Bw=%f, DropRssi=%i, DropPack=%i\n",rssi->getOpen(),sink->rxLast,sink->rxCount,sink->rxBytes,bw,rssi->getDropCount(),pack->getDropCount());
-
-      // rglein
-      //rogue::interfaces::stream::FramePtr frame = rogue::interfaces::stream::Frame::create();
-      //printf("acceptFrame=%l\n",frame.use_count()); //send->myGenFrame(data,100);
-      // gettimeofday(&curr1,NULL);
-      // timersub(&curr1,&last1,&diff1);
-      // diffBytes1 = sink1->rxBytes - lastBytes1;
-      // lastBytes1 = sink1->rxBytes;
-      // timeDiff1 = (double)diff1.tv_sec + ((double)diff1.tv_usec / 1e6);
-      // bw1 = (((float)diffBytes1 * 8.0) / timeDiff1) / 1e9;
-      // gettimeofday(&last1,NULL);
-      // printf("RSSI = %i. RxLast=%i, RxCount=%i, RxTotal=%li, Bw=%f, DropRssi=%i, DropPack=%i\n",rssi1->getOpen(),sink1->rxLast,sink1->rxCount,sink1->rxBytes,bw1,rssi1->getDropCount(),pack1->getDropCount());
    }
 }
